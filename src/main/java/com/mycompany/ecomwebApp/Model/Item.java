dd58d9b46036d4910bcecc8bc06b84770194bf02
Ansar142 <130049687+Ansar142@users.noreply.github.com>
2025-05-12 09:16:18 +0500

    Update Item.java

--- src/main/java/com/mycompany/ecomwebApp/Model/Item.java
+++ src/main/java/com/mycompany/ecomwebApp/Model/Item.java
@@ -26,9 +26,84 @@ import java.util.Date;
         private String brand;
         private BigDecimal price;
         private String category;
+        @JsonFormat(pattern = "yyyy-MM-dd")
         private Date releaseDate;
         private boolean productAvailable;
         private int stockQuantity;
+
+        
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(Date releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public boolean isProductAvailable() {
+        return productAvailable;
+    }
+
+    public void setProductAvailable(boolean productAvailable) {
+        this.productAvailable = productAvailable;
+    }
+
+    public int getStockQuantity() {
+        return stockQuantity;
+    }
+
+    public void setStockQuantity(int stockQuantity) {
+        this.stockQuantity = stockQuantity;
+    }
+
 
     }
 
