a38dd947a52ff87bfde40b77d522ddc04cdac829
Ansar142 <130049687+Ansar142@users.noreply.github.com>
2025-05-13 13:49:23 +0500

    Update Controller.java
    All CRUD operations are added

--- src/main/java/com/mycompany/ecomwebApp/Controller.java
+++ src/main/java/com/mycompany/ecomwebApp/Controller.java
@@ -5,12 +5,11 @@ import com.mycompany.ecomwebApp.Service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
@@ -22,8 +21,44 @@ public class Controller {
     @GetMapping("/products")
     public ResponseEntity<List<Item>> getProducts() {
             return new ResponseEntity<>(productService.getAllProducts(), HttpStatus.ACCEPTED) ;
+
+    }
+    @GetMapping("/product/{getItem}")
+    public ResponseEntity<?> getProducts(@PathVariable("getItem") int getItem) {
+        return new ResponseEntity<>(productService.getAProduct(getItem), HttpStatus.ACCEPTED) ;
+    }
+
+    @PostMapping("/product")
+    public ResponseEntity<?> addProduct(@RequestPart Item product , @RequestPart MultipartFile imageFile){
+        return new ResponseEntity<>(productService.addorUpdateProduct(product,imageFile),HttpStatus.CREATED);
+    }
+    @GetMapping("/product/{id}/image")
+    public ResponseEntity<byte[]> getImageByProductId(@PathVariable int id){
+        Item product = productService.getAProduct(id);
+        return new ResponseEntity<>(product.getImageData(), HttpStatus.OK);
+    }
+    @PutMapping("/product/{id}")
+    public ResponseEntity<?> updateProduct(@PathVariable int id , @RequestPart Item product , @RequestPart MultipartFile imageFile){
+        return new ResponseEntity<>(productService.addorUpdateProduct(product,imageFile),HttpStatus.OK);
+    }
+    @DeleteMapping("/product/{id}")
+    public ResponseEntity<?> deleteProduct(@PathVariable int id) {
+        Item item = productService.getProductByid(id);
+
+        if (item!=null) {
+            productService.deleteProduct(id);
+            return new ResponseEntity<>("Deleted", HttpStatus.OK);
         }
+        else {
+            return new ResponseEntity<>( HttpStatus.NOT_FOUND);
+
+        }
+
     }
 
 
+
+
+
+}
 
