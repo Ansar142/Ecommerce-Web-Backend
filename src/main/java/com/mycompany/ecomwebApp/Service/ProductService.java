461347f4a7c8cee7cbca2c3d633fdb735da229db
Ansar142 <130049687+Ansar142@users.noreply.github.com>
2025-05-13 13:50:06 +0500

    Update ProductService.java

--- src/main/java/com/mycompany/ecomwebApp/Service/ProductService.java
+++ src/main/java/com/mycompany/ecomwebApp/Service/ProductService.java
@@ -1,11 +1,14 @@
 package com.mycompany.ecomwebApp.Service;
 
 import com.mycompany.ecomwebApp.Model.Item;
+import com.mycompany.ecomwebApp.Repository.ProductRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import com.mycompany.ecomwebApp.Repository.ProductRepo;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProductService {
@@ -13,7 +16,26 @@ public class ProductService {
     @Autowired
     private ProductRepo productRepo;
 
+    public Item getProductByid(int product){
+        return productRepo.findById(product).get();
+    }
+
     public List<Item> getAllProducts() {
         return productRepo.findAll();
+    }
+
+    public Item addorUpdateProduct(Item i , MultipartFile imageFile ){
+        i.setImageName(imageFile.getOriginalFilename());
+        i.setImageType(imageFile.getContentType());
+        try {
+            i.setImageData(imageFile.getBytes());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return productRepo.save(i) ;
+    }
+
+    public void deleteProduct(int id){
+        productRepo.deleteById(id);
     }
 }
